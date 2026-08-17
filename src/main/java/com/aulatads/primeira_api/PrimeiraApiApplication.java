26ad85017c3a0dbc338c08c4be93f220e8381236
Vinicius Alves Soares de Melo <viniasm13@gmail.com>
2026-08-17 13:55:56 +0000

    primeiro commit

--- src/main/java/com/aulatads/primeira_api/PrimeiraApiApplication.java
+++ src/main/java/com/aulatads/primeira_api/PrimeiraApiApplication.java
@@ -2,12 +2,20 @@ package com.aulatads.primeira_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PrimeiraApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiraApiApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Olá Mundo!";
 	}
 
 }
